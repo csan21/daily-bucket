866bdd218eaffa7dac7a488ad59fae79fe092337
csan21 <chrisswho@gmail.com>
2017-08-16 19:42:55 -0500

    Play with state and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import { database } from './firebase';
 import rlogo from './rlogo.svg';
 import flogo from './flogo.svg';
 import './App.css';
+import './index.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      newData: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    database.ref('/').on('value', snapshot => {
+      this.setState({
+        data: snapshot.val()
+      });
+    });
+  }
+
+  handleChange(event) {
+    const newData = event.target.value;
+    this.setState({
+      newData
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const newData = database.ref().child('NEWNEW').push(this.state.newData);
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,7 +46,13 @@ class App extends Component {
             <h1>Daily Bucket</h1>
           </div>
         </div>
-        <p className="App-intro">content here!</p>
+        <pre className="App-intro">
+          {JSON.stringify(this.state.data, 2)}
+        </pre>
+        <form className="App-form" onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
